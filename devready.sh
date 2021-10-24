1723c397bbcfc02a6098b6aedff126adef2daee2
Arghya Sarkar <76429451+arghyagod-coder@users.noreply.github.com>
2021-10-24 23:29:27 +0530

    Updated devscript to avoid abscumped errors

--- devready.sh
+++ devready.sh
@@ -7,16 +7,15 @@
 
 # Arcolinux:
 
-sudo pacman -S wget --noconfirm --needed
+pacman -S wget --noconfirm --needed
 
 echo "Getting the ArcoLinux keys from the ArcoLinux repo"
-
-sudo wget https://github.com/arcolinux/arcolinux_repo/raw/master/x86_64/arcolinux-keyring-20230919-6-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
+wget https://github.com/arcolinux/arcolinux_repo/raw/master/x86_64/arcolinux-keyring-20230919-6-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
+pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
 
 echo "Getting the latest arcolinux mirrors file"
 
-sudo wget https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist -O /etc/pacman.d/arcolinux-mirrorlist
+wget https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist -O /etc/pacman.d/arcolinux-mirrorlist
 
 # Chaotic AUR
 echo "Fetching chaotic keys"
@@ -26,17 +25,5 @@ pacman-key --lsign-key 3056513887B78AEB
 echo "Getting the latest chaotic mirrorlist"
 
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-sudo cp etc/pacman.d/chaotic-mirrorlist /etc/pacman.d/chaotic-mirrorlist
+cp etc/pacman.d/chaotic-mirrorlist /etc/pacman.d/chaotic-mirrorlist
 
-echo '
-[arcolinux_repo_3party]
-SigLevel = Required DatabaseOptional
-Include = /etc/pacman.d/arcolinux-mirrorlist
-[arcolinux_repo_xlarge]
-SigLevel = Required DatabaseOptional
-Include = /etc/pacman.d/arcolinux-mirrorlist
-[calinix]
-SigLevel = Optional TrustedOnly
-Server = https://get.calinix.tech/calinix_repo/$arch
-[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee --append /etc/pacman.conf
