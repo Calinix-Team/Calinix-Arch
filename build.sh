7c250fd18c55c5b08e800ecb14a436744d3ac328
Arghya Sarkar <arghyasarkar.nolan@gmail.com>
2021-12-01 14:41:02 +0530

    Updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+pacman -S --noconfirm --needed archlinux-keyring
+pacman -S --needed --noconfirm archiso mkinitcpio-archiso
+
+
 mkarchiso -v -w ./work -o ./out ./archiso/
 
 echo "ISO Built in ./out folder!"
