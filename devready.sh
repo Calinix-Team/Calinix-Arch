230ffdc4e5a44ad344813700da11ae99747ed0fd
Arghya Sarkar <76429451+arghyagod-coder@users.noreply.github.com>
2021-12-10 01:26:17 +0530

    Fixed chaotic aur

--- devready.sh
+++ devready.sh
@@ -23,7 +23,8 @@ pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 
 echo "Getting the latest chaotic mirrorlist"
-
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+wget https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
+wget https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst
+pacman -U chaotic-keyring.pkg.tar.zst' --noconfirm
 cp archiso/airootfs/etc/pacman.d/chaotic-mirrorlist /etc/pacman.d/chaotic-mirrorlist
 
